199c9fe3e45553a5f97b6965dc33252f934f2a57
elissamunanira <munanira64@gmail.com>
2024-04-26 18:58:21 +0200

    some changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server listening at port ${process.env.PORT}`);
+  console.log(`Connecting to database ...`);
 });
